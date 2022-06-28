cd9a66496b372c4414175eb8564a1437b11251fa
Phu Nguyen <diemduyen05@gmail.com>
2022-06-28 07:47:37 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function() {
+    var $pane = $('.wrapper__bnf__carosel');
+    if ( $(window).width() < 768 ) {
+        startCarousel();
+    } else {
+        $('.owl-carousel').addClass('off');
+    }
+    $(window).resize(function() {
+        if ( $(window).width() < 768 ) {
+          startCarousel();
+        } else {
+          stopCarousel();
+        }
+    });
+    function startCarousel (){
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin: 30, // marfin giữa các hình ảnh
+            nav: false,  // Cho phép hiển thị pre/next button
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            },
+            dots: true,
+            autoplay:false,
+            autoplayTimeout:4000,
+            autoplayHoverPause:true
+        })
+    }
+    function stopCarousel() {
+        var owl = $('.owl-carousel');
+        owl.trigger('destroy.owl.carousel');
+        owl.addClass('off');
+      }
+});
